00fa9b82443c0bc9e1372a55e75d7f3341cbee5d
DmitryShutov <7dmitry77@gmail.com>
2016-03-16 12:19:55 -0400

    Ajax request is working!

--- weather_api/script.js
+++ weather_api/script.js
@@ -1,14 +1,33 @@
-document.onload = function() {
+var long, lat;
+window.onload = function() {
+ coords(function (arr) {
+    var xhr = new XMLHttpRequest();
+    var key = '6e7e0a9ed33f6ac9067b4121d1f6b1d9';
+    var request = 'http://api.openweathermap.org/data/2.5/weather?lat=' + arr[0] + '&lon=' + arr[1] + '&APPID=' + key;  
+    xhr.open('GET', request, false);
+    xhr.send();
     
-}
+    if(xhr.status != 200) {
+         alert( xhr.status + ': ' + xhr.statusText );
+    } else {
+        alert( xhr.responseText);
+    }
+});   
+     
+};
 
-function coords() {
+
+
+function coords(callback) {
     if("geolocation" in navigator) {
        navigator.geolocation.getCurrentPosition(function(position){
-           
-       });
+           long = position.coords.longitude;
+           lat = position.coords.latitude;
+           var curLocation = [lat, long];
+           callback(curLocation); 
+       }); 
     } else {
-        document.querySelector('.loc').innerHTML = "Sorry, we can't locate you";
-        
+        document.querySelector('.loc').innerHTML = "Sorry, we can't locate you";        
     }
-}
+};
+
